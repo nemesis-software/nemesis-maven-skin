bfa59f82bd70a3b2c6289a0a184fa9cc491c77cb
petar.tahchiev <paranoiabla@gmail.com>
2017-06-10 09:02:29 +0300

    Improve rest rester =.

--- src/main/resources/js/script.js
+++ src/main/resources/js/script.js
@@ -16,34 +16,39 @@ $(function(){
 			}, 500);
 		},
 		restTester: {
-			requestType: $('.rest-tester [name="client_type"]:checked').val(),
+			clientType: $('.rest-tester [name="client_type"]:checked').val(),
 			hostname: window.location.href.replace(/(^\w+:|^)\/\//, '').split('/')[0],
-			urlTemplate: '/current-snapshot/platform/modules/nemesis-module-restservices/__entity__/__verb__-example/__client_type__-request.html',
+			coreUrlTemplate: '/current-snapshot/platform/modules/nemesis-module-restservices/__entity__/__verb__-example/__client_type__-request.html',
+			facadeUrlTemplate: '/current-snapshot/platform/nemesis-platform-facade/__class__/__method__/__client_type__-request.html',
 
 			/**
 			 * Initialize rest tester object
+			 *
 			 * @return {void}
 			 * @author Vladimir Kuzmov <me@vkuzmov.com>
 			 */
 			init: function() {
-
 				switch(this.hostname) {
 					case 'nemesis-new-site.dev':
-						this.urlTemplate = '/docs'+this.urlTemplate;
+						this.coreUrlTemplate = '/docs'+this.coreUrlTemplate;
+						this.facadeUrlTemplate = '/docs'+this.facadeUrlTemplate;
 						break;
 					case 'vkuzmov.com':
-						this.urlTemplate = '/nemesis-new-site/current/docs'+this.urlTemplate;
+						this.coreUrlTemplate = '/nemesis-new-site/current/docs'+this.coreUrlTemplate;
+						this.facadeUrlTemplate = '/nemesis-new-site/current/docs'+this.facadeUrlTemplate;
 						break;
 				}
 
 				$('.rest-tester [name="client_type"]').on('change', function(e) {
-					nemesis.restTester.requestType = $(this).val();
+					nemesis.restTester.clientType = $(this).val();
 					$('.rest-tester .response-wrapper .response').remove();
 				});
 
-				$('.rest-tester .entity').on('change', function() {
+				$('.rest-tester [data-api-trigger="true"]').on('change', function() {
 					nemesis.restTester.loadContent($(this).closest('.tab-pane'));
 				});
+
+				$('.rest-tester [data-api-object="method"]').chainedTo('.rest-tester [data-api-object="class"]');
 
 				// Remove response
 				$('.rest-tester').on('click', '.response .remove', function(e) {
@@ -60,24 +65,20 @@ $(function(){
 
 			/**
 			 * Load external content
+			 *
+			 * @param {object} parent
 			 * @return {void}
 			 * @author Vladimir Kuzmov <me@vkuzmov.com>
 			 */
 			loadContent: function(parent) {
-				var verb = $('.verb', parent).val(),
-					entity = $('.entity', parent).val();
+				var endpoint = this.getEndpoint(parent);
 
-				if ( ! verb || ! entity) {
+				if (endpoint === false) {
 					return;
 				}
 
-				var url = this.urlTemplate
-					.replace('__client_type__', this.requestType)
-					.replace('__verb__', verb)
-					.replace('__entity__', entity);
-
 				$.ajax({
-					url: url,
+					url: endpoint,
 					type: 'GET',
 					cache: false,
 					success: function(response, data) {
@@ -99,22 +100,52 @@ $(function(){
 
 			/**
 			 * Get endpoint for external content
-			 * @return {string}
+			 *
+			 * @param  {object} parent
+			 * @return {string|boolean} boolean when fails
 			 * @author Vladimir Kuzmov <me@vkuzmov.com>
 			 */
-			getEndpoint: function() {
-				return this.urlTemplate
-					.replace('__client_type__', this.requestType)
-					.replace('__verb__', this.verb.val())
-					.replace('__entity__', this.entity.val());
+			getEndpoint: function(parent) {
+				var activeTab = parent.attr('id'),
+					first = $('[data-api-object]:eq(0)', parent),
+					second = $('[data-api-object]:eq(1)', parent);
+
+				if ( ! first.val() || ! second.val()) {
+					return false;
+				}
+
+				return this[activeTab+'UrlTemplate']
+					.replace('__client_type__', this.clientType)
+					.replace('__'+first.data('apiObject')+'__', first.val())
+					.replace('__'+second.data('apiObject')+'__', second.val());
 			},
 
+			/**
+			 * Update X-Nemesis-Token
+			 *
+			 * @param  {object} content
+			 * @return {void}
+			 * @author Vladimir Kuzmov <me@vkuzmov.com>
+			 */
 			updateToken: function(content) {
 				var tokenValue = $('#nemesis_token').val(),
-					newToken = "-H 'X-Nemesis-Token: "+tokenValue+"'",
 					code = $('code', content).text();
 
-				code = code.replace(/-H 'X-Nemesis-Token: (.*)'/gi, newToken);
+				switch(this.clientType) {
+					case 'curl':
+						code = code.replace(/-H 'X-Nemesis-Token:[^']+'/gi, "-H 'X-Nemesis-Token: "+tokenValue+"'");
+						break;
+					case 'httpie':
+						code = code.replace(/'X-Nemesis-Token:[^']+'/gi, "'X-Nemesis-Token: "+tokenValue+"'");
+						break;
+					case 'http':
+						code = code.replace(/X-Nemesis-Token:[^\n]+/gi, 'X-Nemesis-Token: '+tokenValue);
+						break;
+					default:
+						return;
+				}
+
+				code = $('<div>').text(code).html();
 				$('code', content).html(code);
 			}
 		}
